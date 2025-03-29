1996ede9658928d6b8d08ff4d35e387a35d40750
kaiobrunobm <bmkaiobruno@gmail.com>
2025-03-29 18:13:09 -0300

    Ending of the project, adding image and icons with personal and professionals informations

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -22,7 +22,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -91,10 +90,6 @@ fun TitlePicture (modifier: Modifier = Modifier) {
             Image(
                 painter = image,
                 contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .size(260.dp)
-
             )
     }
 
